7f21a8b9341f5abd8de7fa225763b3ab46cdd362
alexcatlow <alex@polimonitor.com>
2023-06-07 09:55:22 +0100

    Update Serializers

--- bunnies/serializers.py
+++ bunnies/serializers.py
@@ -4,16 +4,12 @@ from bunnies.models import Bunny, RabbitHole
 
 
 class RabbitHoleSerializer(serializers.ModelSerializer):
-    # this is returning 4 and it should be 3 , it is counting all the bunnies in the database
     bunnies = serializers.PrimaryKeyRelatedField(many=True, queryset=Bunny.objects.all())
-    # bunnies = 3
     bunny_count = serializers.SerializerMethodField()
-    # bunny_count = 3
-    # print(bunny_count)
 
     def get_bunny_count(self, obj):
-        return 3
-        # return Bunny.objects.count()
+        bunnies_in_hole = [b for b in Bunny.objects.all() if b.home == obj]
+        return len(bunnies_in_hole)
 
     class Meta:
         model = RabbitHole
@@ -34,4 +30,3 @@ class BunnySerializer(serializers.ModelSerializer):
     class Meta:
         model = Bunny
         fields = ('name', 'home', 'family_members')
-
